2250fbd270aa0af23f996a312b5f080bfef46ad9
Ivo Sabev <ivolution@gmail.com>
2024-01-25 10:29:08 +0200

    Fix bug with wrong placement of eslint config prettier

--- index.js
+++ index.js
@@ -19,60 +19,62 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 // const ERROR = 2;
 
 /** @type {import('eslint').Linter.Config} */
-const config = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
+const config = [
+  {
+    env: {
+      browser: true,
+      commonjs: true,
+      es6: true,
+    },
+    extends: ['eslint:recommended', 'prettier', 'plugin:import/recommended'],
+    overrides: [
+      {
+        extends: ['plugin:import/typescript', 'plugin:@typescript-eslint/recommended'],
+        files: ['**/*.ts?(x)'],
+        parser: '@typescript-eslint/parser',
+        parserOptions: {
+          ecmaVersion: 2019,
+          sourceType: 'module',
+        },
+        plugins: ['@typescript-eslint'],
+        rules: {
+          ...typescriptRules,
+        },
+      },
+    ],
+    parser: '@babel/eslint-parser',
+    parserOptions: {
+      babelOptions: {
+        presets: [require.resolve('@babel/preset-react')],
+      },
+      ecmaVersion: 'latest',
+      requireConfigFile: false,
+      sourceType: 'module',
+    },
+
+    plugins: ['prettier', 'import', 'react', 'react-hooks', 'jsx-a11y', 'sort-destructure-keys', 'sort-keys-fix'],
+    // NOTE: In general - we want to use prettier for the majority of stylistic
+    // concerns.  However there are some "stylistic" eslint rules we use that should
+    // not fail a PR since we can auto-fix them after merging to dev.  These rules
+    // should be set to WARN.
+    //
+    // ERROR should be used for "functional" rules that indicate a problem in the
+    // code, and these will cause a PR failure
+    // IMPORTANT: Ensure that rules used here are compatible with
+    // typescript-eslint. If they are not, we need to turn the rule off in our
+    // overrides for ts/tsx.
+    // To read the details for any rule, see https://eslint.org/docs/rules/[RULE-KEY]
+    rules: {
+      ...coreRules,
+      ...importRules,
+      ...reactRules,
+      ...jsxA11yRules,
+    },
+    settings: {
+      ...importSettings,
+    },
   },
   eslintConfigPrettier,
-  extends: ['eslint:recommended', 'prettier', 'plugin:import/recommended'],
-  overrides: [
-    {
-      extends: ['plugin:import/typescript', 'plugin:@typescript-eslint/recommended'],
-      files: ['**/*.ts?(x)'],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        ecmaVersion: 2019,
-        sourceType: 'module',
-      },
-      plugins: ['@typescript-eslint'],
-      rules: {
-        ...typescriptRules,
-      },
-    },
-  ],
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    babelOptions: {
-      presets: [require.resolve('@babel/preset-react')],
-    },
-    ecmaVersion: 'latest',
-    requireConfigFile: false,
-    sourceType: 'module',
-  },
-
-  plugins: ['prettier', 'import', 'react', 'react-hooks', 'jsx-a11y', 'sort-destructure-keys', 'sort-keys-fix'],
-  // NOTE: In general - we want to use prettier for the majority of stylistic
-  // concerns.  However there are some "stylistic" eslint rules we use that should
-  // not fail a PR since we can auto-fix them after merging to dev.  These rules
-  // should be set to WARN.
-  //
-  // ERROR should be used for "functional" rules that indicate a problem in the
-  // code, and these will cause a PR failure
-  // IMPORTANT: Ensure that rules used here are compatible with
-  // typescript-eslint. If they are not, we need to turn the rule off in our
-  // overrides for ts/tsx.
-  // To read the details for any rule, see https://eslint.org/docs/rules/[RULE-KEY]
-  rules: {
-    ...coreRules,
-    ...importRules,
-    ...reactRules,
-    ...jsxA11yRules,
-  },
-  settings: {
-    ...importSettings,
-  },
-};
+];
 
 module.exports = config;
