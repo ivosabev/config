a82fef37360e2b8e86983e5c32cf6bfb3dd64c55
Ivo Sabev <ivolution@gmail.com>
2024-02-15 11:22:21 +0200

    Guard against Method Shorthand Syntax

--- rules/typescript.js
+++ rules/typescript.js
@@ -3,6 +3,7 @@ module.exports = {
   '@typescript-eslint/ban-types': 'off',
   '@typescript-eslint/consistent-type-assertions': 'warn',
   '@typescript-eslint/consistent-type-imports': 'warn',
+  '@typescript-eslint/method-signature-style': ['error', 'property'],
   '@typescript-eslint/no-empty-function': 'off',
   '@typescript-eslint/no-empty-interface': 'off',
   '@typescript-eslint/no-explicit-any': 'off',
