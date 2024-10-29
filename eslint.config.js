8a4472b9a2fcf65084109a8f0a88c81c6ed2cbb8
Ivo Sabev <ivolution@gmail.com>
2024-10-29 14:09:30 +0200

    Ignore _old folder in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,9 @@
 import {config as defaultConfig} from './eslint.js';
 
 /** @type {import("eslint").Linter.Config} */
-export default [...defaultConfig];
+export default [
+  ...defaultConfig,
+  {
+    ignores: ['_old/'],
+  },
+];
