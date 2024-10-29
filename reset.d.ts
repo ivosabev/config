8fcfafef04bd6db4b7e49b5af993b3670defa0d7
Ivo Sabev <ivolution@gmail.com>
2024-10-29 11:57:10 +0200

    Use spaces

--- reset.d.ts
+++ reset.d.ts
@@ -4,8 +4,8 @@ import '@total-typescript/ts-reset/dom';
 import 'react';
 
 declare module 'react' {
-	// support css variables
-	interface CSSProperties {
-		[key: `--${string}`]: string | number;
-	}
+  // support css variables
+  interface CSSProperties {
+    [key: `--${string}`]: string | number;
+  }
 }
