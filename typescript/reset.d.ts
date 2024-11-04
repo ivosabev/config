7f0061bf9069a36ef83552c6d1029b93647633d6
Ivo Sabev <ivolution@gmail.com>
2024-11-04 16:55:20 +0200

    Add SVG type definitions

--- typescript/reset.d.ts
+++ typescript/reset.d.ts
@@ -13,3 +13,7 @@ declare module '*.css' {
   const styles: {[className: string]: string};
   export default styles;
 }
+declare module '*.svg' {
+  import type * as React from 'react';
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & {title?: string}>;
+}
